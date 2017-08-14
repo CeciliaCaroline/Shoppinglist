7378d835815803da09652ab91c5caa775177c99e
CeciliaCaroline <nalubegac58@gmail.com>
2017-08-15 02:28:29 +0300

    Added method to create and edit list

--- challenge1/models/user.py
+++ challenge1/models/user.py
@@ -12,4 +12,32 @@ class User:
         self.lists = {}
         self.new_lists = {}
         self.list_items = {}
-        
+
+    def create_list(self, list):
+        """"
+        method to check if the list id exists, if not,  create a new list
+        :param list
+        """
+        if list.list_id in self.lists.keys():
+            return False
+        elif list.title in self.new_lists:
+            return False
+
+        else:
+            self.lists[list.list_id] = list
+            self.new_lists[list.list_id] = list.title
+            return True
+
+    def edit_list(self, list_id, title, description):
+        """"
+        method to edit the title of an existing bucket list
+        :param list_id
+        :param title
+        :param description
+        """
+        if list_id in self.lists.keys():
+            edited_list = self.lists[list_id]
+            edited_list.title = title
+            edited_list.description = description
+            return True
+        return False
