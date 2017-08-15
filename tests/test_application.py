9f885bfcc99bf332c23f952270611d6b7e16b02c
CeciliaCaroline <nalubegac58@gmail.com>
2017-08-15 03:17:02 +0300

    Added tests for invalid inputs at signup and login

--- tests/test_application.py
+++ tests/test_application.py
@@ -20,6 +20,33 @@ class TestApplication(unittest.TestCase):
         self.app.register(self.user)
         self.assertTrue(self.app.login('cecilia@gmail.com', '123456'))
 
+    def test_invalid_email_type_at_signup(self):
+        self.user = self.app.register(self.user)
+        self.user1 = User('CeciliaCaroline', 'cecilia', '123456')
+        new_user = self.app.register(self.user1)
+        self.assertEqual(new_user, self.user, msg='Input is not an email')
+
+    def test_for_empty_password_at_signup(self):
+        self.user = self.app.register(self.user)
+        self.user1 = User('CeciliaCaroline', 'cecilia', ' ')
+        new_user = self.app.register(self.user1)
+        self.assertEqual(new_user, self.user, msg='Password is empty')
+
+    def test_invalid_email_type_at_login(self):
+        self.user = self.app.login('cecilia@gmail.com', '123456')
+        new_user = self.app.login('cecilia.com', '123456')
+        self.assertEqual(new_user, self.user, msg='Input is not an email')
+
+    def test_for_empty_password_at_login(self):
+        self.user = self.app.login('cecilia@gmail.com', '123456')
+        new_user = self.app.login('cecilia@gmail.com', ' ')
+        self.assertEqual(new_user, self.user, msg='Password is empty')
+
+    def test_for_invalid_password_at_login(self):
+        self.user1 = self.app.login('cecilia@gmail.com', '123456')
+        new_user = self.app.login('cecilia@gmail.com', 'pass ')
+        self.assertEqual(new_user, self.user1, msg='The password is invalid')
+
 
 if __name__ == '__main__':
     unittest.main()
